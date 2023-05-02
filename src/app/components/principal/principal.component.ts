3bdc3c2c34db63a846d253b1996b6e442e4e80de
AndreeCarrillo <u20211b589@upc.edu.pe>
2023-05-02 01:48:19 -0500

    feat: filters

--- src/app/components/principal/principal.component.ts
+++ src/app/components/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-principal',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class PrincipalComponent {
   ex:string ="Luis Alberto"
+  property_Filter!: string;
+  accommodation_Filter!: string;
+  guests_Filter: number= 0;
+  bedrooms_Filter: number = 0;
+  bathrooms_Filter: number = 0;
+  services_Filter!: string[];
+  modcons_Filter!: string[];
+  features_Filter!: string[];
+  price_Filter: number = 0;
+  
+  properties_array!: string[];
+  accommodations_array!: string[];
+  services_array: string[] =[
+    "Wifi",
+    "Food",
+    "Jacuzzi",
+    "Sillon Tantrico"
+  ];
+  modcons_array!: string[];
+  features_array!: string[];
+  price_array: string[] =[
+    "desde S/0 hasta S/100",
+    "desde S/100 hasta S/300",
+    "desde S/300 hasta S/700",
+    "desde S/700 hasta S/1500",
+    "desde S/1500 hasta S/2500",
+    "desde S/2500 hasta S/5000",
+    "desde S/5000 hasta S/10000",
+    "desde S/10000",
+  ] ;
+
 }
