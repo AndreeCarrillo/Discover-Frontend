7c126c9e5084fad598365eeb7c1195abaf5b90b9
David5761 <75706282+David5761@users.noreply.github.com>
2023-05-03 00:14:03 -0500

    feat:Edit Make opinion

---
+++ src/app/components/make-opinion/make-opinion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-make-opinion',
+  templateUrl: './make-opinion.component.html',
+  styleUrls: ['./make-opinion.component.scss']
+})
+export class MakeOpinionComponent {
+  
+}
