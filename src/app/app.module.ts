dff485df542ca3a4e4091f91b063de91c12c6234
David5761 <75706282+David5761@users.noreply.github.com>
2023-05-02 23:15:17 -0500

    feat: - Add interface make opinion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,8 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { PropertyCardComponent } from './components/property-card/property-card.component';
 import { VisualizeItemComponent } from './components/visualize-item/visualize-item.component';
-import { PublicarInmuebleComponent } from './components/publicar-inmueble/publicar-inmueble.component'
+import { PublicarInmuebleComponent } from './components/publicar-inmueble/publicar-inmueble.component';
+import { MakeOpinionComponent } from './components/make-opinion/make-opinion.component'
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { PublicarInmuebleComponent } from './components/publicar-inmueble/public
     PrincipalComponent,
     PropertyCardComponent,
     VisualizeItemComponent,
-    PublicarInmuebleComponent
+    PublicarInmuebleComponent,
+    MakeOpinionComponent
   ],
   imports: [
     BrowserModule,
